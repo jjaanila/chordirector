57958fc2ac05a263dbdc88751b622e647d75ff05
Janne Jaanila <janne.jaanila@gmail.com>
2021-02-13 13:30:13 +0200

    Change to fixed controls. Start implementing countdown

--- src/index.tsx
+++ src/index.tsx
@@ -20,6 +20,12 @@ const controlCss = {
   marginRight: '4px',
 };
 
+const controlsCss = {
+  position: 'fixed',
+  top: '0.1rem',
+  right: '0.1rem',
+};
+
 const Chord = ({ chord }: { chord: TokenChord }) => {
   return (
     <strong>
@@ -34,28 +40,42 @@ const NonChord = ({ value }: { value: Token['value'] }) => {
   return <span style={nonChordCss}>{value}</span>;
 };
 
-let scrollingInterval: NodeJS.Timeout | null = null;
+let countdownTimeout: NodeJS.Timeout | undefined;
+let countdownInterval: NodeJS.Timeout | undefined;
+let scrollingInterval: NodeJS.Timeout | undefined;
 
 const App: React.FC = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [isScrolling, setIsScrolling] = useState(false);
   const [song, setSong] = useState<Song>({ tokens: [], transposeLevel: 0 });
   const [scrollSpeed, setScrollSpeed] = useState<number>(200);
+  const [countdown, setCountdown] = useState(0);
+  const [countdownSecond, setCountdownSecond] = useState<number>(0);
   const stopScrolling = () => {
-    if (scrollingInterval !== null) {
+    if (scrollingInterval !== undefined) {
       clearInterval(scrollingInterval);
-      scrollingInterval = null;
+      scrollingInterval = undefined;
       setIsScrolling(false);
+    }
+    if (countdownTimeout !== undefined) {
+      clearTimeout(countdownTimeout);
+      countdownTimeout = undefined;
     }
   };
   const startScrolling = (scrollSpeed: number) => {
-    scrollingInterval = setInterval(function () {
-      window.scrollBy(0, scrollSpeed * 0.01);
-      if (window.innerHeight + window.pageYOffset >= document.body.offsetHeight) {
-        stopScrolling();
-      }
-    }, 100);
     setIsScrolling(true);
+    setCountdownSecond(countdown);
+    countdownInterval = setInterval(() => {
+      setCountdownSecond(countdownSecond - 1);
+      if (countdownSecond === 0 && countdownInterval !== undefined) {
+        clearInterval(countdownInterval);
+      }
+    }, 1000);
+    setTimeout(() => {
+      scrollingInterval = setInterval(function () {
+        window.scrollBy(0, scrollSpeed * 0.01);
+      }, 100);
+    }, countdown * 1000);
   };
   useEffect(() => {
     chrome.runtime.onMessage.addListener((request: NewSongRequest, _sender, sendResponse) => {
@@ -75,7 +95,7 @@ const App: React.FC = () => {
   }
   return (
     <main style={mainCss}>
-      <section>
+      <section style={controlsCss}>
         <button style={controlCss} id="transpose-up" onClick={() => setSong(transpose(song, song.transposeLevel + 1))}>
           Transpose up
         </button>
@@ -100,7 +120,7 @@ const App: React.FC = () => {
             }
           }}
         >
-          {!isScrolling ? 'Start scrolling' : 'Stop scrolling'}
+          {countdownSecond > 0 ? countdownSecond : !isScrolling ? 'Start scrolling' : 'Stop scrolling'}
         </button>
         <select
           disabled={isScrolling}
@@ -113,6 +133,15 @@ const App: React.FC = () => {
           <option value={200}>Medium</option>
           <option value={300}>Fast</option>
         </select>
+        <input
+          disabled={isScrolling}
+          id="countdown-input"
+          type="number"
+          value={undefined}
+          placeholder={'Time to wait before scrolling'}
+          title="Time to wait before scrolling"
+          onChange={(event) => setCountdown(parseInt(event.target.value, 10))}
+        />
       </section>
       <section>
         <p style={songCss}>
